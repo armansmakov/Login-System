0af28ba2898816555143dc6e5f63e7bef212e78f
Armanoid <armansmakov321@gmail.com>
2023-11-21 15:16:56 +0600

    Add project files.

---
+++ PetProject/Models/Entities/LoginInfo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PetProject.Models.Entities
+{
+    public class LoginInfo
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ PetProject/Models/Entities/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PetProject.Models.Entities
+{
+    public class UserInfo
+    {
+        public int Id {  get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int LoginId { get; set; }
+        public LoginInfo LoginInfo { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
