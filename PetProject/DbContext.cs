54889a1e412b87d6dc6bceda464e24948dd178e0
Armanoid <armansmakov321@gmail.com>
2023-12-20 20:16:34 +0600

    Добавил класс MyDbContext

---
+++ PetProject/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetProject.Models.Entities;
+
+namespace PetProject
+{
+    public class MyDbContext : DbContext
+    {
+        public IConfiguration _config {  get; set; }
+        public MyDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+        public DbSet<LoginInfo> LoginInfos { get; set; }
+    }
+}
