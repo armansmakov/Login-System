d62bedc1885b369625386649ecffc8de1941fe7f
Armanoid <armansmakov321@gmail.com>
2024-02-19 22:10:15 +0600

    Добавил папку Validation и класс UserInfoValidator

---
+++ PetProject/Validation/UserInfoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PetProject.Models.Entities;
+
+
+namespace PetProject.Validation
+{
+    public class UserInfoValidator : AbstractValidator<UserInfo>
+    {
+        public UserInfoValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty
+        }
+    }
+}
